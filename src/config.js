f93c88de961456c9c2f0d1b1163fe9eba0f35dd3
Suryanshu Choudhary <suryanshuchoudhary14@gmail.com>
2025-07-13 23:57:54 +0530

    Initial clean commit

---
+++ src/config.js
@@ -0,0 +1,54 @@
+export const MAP_STYLE = "./map_style.json";
+
+export const INITIAL_VIEW_STATE = {
+    longitude: 75.7873,
+    latitude:  26.9124 ,
+    zoom: 13,
+    pitch: 10,
+    bearing: 1110
+};
+
+export const INITIAL_COLORS = {
+    startNodeFill: [70, 183, 128],
+    startNodeBorder: [255, 255, 255],
+    endNodeFill: [152, 4, 12],
+    endNodeBorder: [0, 0, 0],
+    path: [70, 183, 128],
+    route: [165, 13, 32],
+};
+
+export const LOCATIONS = [
+  { name: "Jaipur", latitude: 26.9124, longitude: 75.7873 },
+  { name: "Andhra Pradesh", latitude: 15.9129, longitude: 79.7400 },
+  { name: "Arunachal Pradesh", latitude: 28.2180, longitude: 94.7278 },
+  { name: "Assam", latitude: 26.2006, longitude: 92.9376 },
+  { name: "Bihar", latitude: 25.0961, longitude: 85.3131 },
+  { name: "Chhattisgarh", latitude: 21.2787, longitude: 81.8661 },
+  { name: "Goa", latitude: 15.2993, longitude: 74.1240 },
+  { name: "Gujarat", latitude: 22.2587, longitude: 71.1924 },
+  { name: "Haryana", latitude: 29.0588, longitude: 76.0856 },
+  { name: "Himachal Pradesh", latitude: 31.1048, longitude: 77.1734 },
+  { name: "Jharkhand", latitude: 23.6102, longitude: 85.2799 },
+  { name: "Karnataka", latitude: 15.3173, longitude: 75.7139 },
+  { name: "Kerala", latitude: 10.8505, longitude: 76.2711 },
+  { name: "Madhya Pradesh", latitude: 22.9734, longitude: 78.6569 },
+  { name: "Maharashtra", latitude: 19.7515, longitude: 75.7139 },
+  { name: "Manipur", latitude: 24.6637, longitude: 93.9063 },
+  { name: "Meghalaya", latitude: 25.4670, longitude: 91.3662 },
+  { name: "Mizoram", latitude: 23.1645, longitude: 92.9376 },
+  { name: "Nagaland", latitude: 26.1584, longitude: 94.5624 },
+  { name: "Odisha", latitude: 20.9517, longitude: 85.0985 },
+  { name: "Punjab", latitude: 31.1471, longitude: 75.3412 },
+  { name: "Rajasthan", latitude: 27.0238, longitude: 74.2179 },
+  { name: "Sikkim", latitude: 27.5330, longitude: 88.5122 },
+  { name: "Tamil Nadu", latitude: 11.1271, longitude: 78.6569 },
+  { name: "Telangana", latitude: 18.1124, longitude: 79.0193 },
+  { name: "Tripura", latitude: 23.9408, longitude: 91.9882 },
+  { name: "Uttar Pradesh", latitude: 26.8467, longitude: 80.9462 },
+  { name: "Uttarakhand", latitude: 30.0668, longitude: 79.0193 },
+  { name: "West Bengal", latitude: 22.9868, longitude: 87.8550 },
+  { name: "Delhi", latitude: 28.6139, longitude: 77.2090 },
+  { name: "Jammu and Kashmir", latitude: 33.7782, longitude: 76.5762 },
+  { name: "Ladakh", latitude: 34.1526, longitude: 77.5770 },
+  
+];
